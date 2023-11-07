f6a963330d628911ee2703a20c91654993989f99
unknown <raulag891@gmail.com>
2023-11-07 15:05:22 +0100

    Solución proyecto

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 const button = document.querySelector(".button_main");
+const latitude = localStorage.getItem("latitude");
+const longitude = localStorage.getItem("longitude");
 
 button.addEventListener("click", function () {
-  if ("geolocation" in navigator) {
-    button.disabled = true;
-    navigator.geolocation.getCurrentPosition(function (position) {
-      const latitude = position.coords.latitude;
-      const longitude = position.coords.longitude;
+  if (!latitude && !longitude) {
+    if ("geolocation" in navigator) {
+      button.disabled = true;
+      navigator.geolocation.getCurrentPosition(function (position) {
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
 
-      localStorage.setItem("latitude", latitude);
-      localStorage.setItem("longitude", longitude);
-    });
-    window.location.href = "forecast.html";
-    function mensgeError(error) {
-      console.error("Error al obtener la ubicación: ", error.message);
+        localStorage.setItem("latitude", latitude);
+        localStorage.setItem("longitude", longitude);
+        window.location.href = "forecast.html";
+      });
+      function mensgeError(error) {
+        console.error("Error al obtener la ubicación: ", error.message);
+      }
+    } else {
+      console.error("La geolocalización no esta disponible en este navegador.");
     }
   } else {
-    console.error("La geolocalización no esta disponible en este navegador.");
+    window.location.href = "forecast.html";
   }
 });
